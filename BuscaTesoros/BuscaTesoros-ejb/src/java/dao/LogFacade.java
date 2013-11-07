3881f68d18ab38326d4c549671f89f5237e9ff0f
Jorge Peña <nosisomes@gmail.com>
2013-11-07 14:17:15 +0100

    First commit: Proyecto con las entidades y beans incluidos

---
+++ BuscaTesoros/BuscaTesoros-ejb/src/java/dao/LogFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+
+import entity.Log;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author wkynrocks
+ */
+@Stateless
+public class LogFacade extends AbstractFacade<Log> {
+    @PersistenceContext(unitName = "BuscaTesoros-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public LogFacade() {
+        super(Log.class);
+    }
+    
+}
---
+++ BuscaTesoros/BuscaTesoros-ejb/src/java/dao/TesoroFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+
+import entity.Tesoro;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author wkynrocks
+ */
+@Stateless
+public class TesoroFacade extends AbstractFacade<Tesoro> {
+    @PersistenceContext(unitName = "BuscaTesoros-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public TesoroFacade() {
+        super(Tesoro.class);
+    }
+    
+}
