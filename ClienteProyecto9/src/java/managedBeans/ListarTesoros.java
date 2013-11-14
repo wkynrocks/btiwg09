88ec803bc58bbf04bcfaa5e8c4e4c7ab959bcd5f
Jorge Peña <nosisomes@gmail.com>
2013-11-14 16:06:36 +0100

    Merge origin/master
    Conflicts:
    	ClienteProyecto9/src/java/managedBeans/ListarTesoros.java

--- ClienteProyecto9/src/java/managedBeans/ListarTesoros.java
+++ ClienteProyecto9/src/java/managedBeans/ListarTesoros.java
@@ -152,13 +152,4 @@ public class ListarTesoros {
         port.editTesoro(entity);
     }
     
-
-    private void editTesoro(service.Tesoro entity) {
-        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
-        // If the calling of port operations may lead to race condition some synchronization is required.
-        service.TesoroService port = service.getTesoroServicePort();
-        port.editTesoro(entity);
-    }
-    
-
 }
