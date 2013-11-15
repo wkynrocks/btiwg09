8eebf0fec6fc9268e6657e820f5168aac8b7d50a
Jorge Peña <nosisomes@gmail.com>
2013-11-15 11:41:31 +0100

    sql modificado, cleanear runear todo de nuevo

--- scriptSQL.sql
+++ scriptSQL.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `proyecto9`.`User` (
   `username` VARCHAR(16) NOT NULL,
   `password` VARCHAR(32) NOT NULL,
   `email` VARCHAR(255) NOT NULL,
-  `rol` ENUM('BuscaTesoros','Colaborador','Admin') NOT NULL,
+  `rol` ENUM('BuscaTesoros','Colaborador','Admin','Deshabilitado') NOT NULL,
   UNIQUE INDEX `username_UNIQUE` (`username` ASC),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC),
   PRIMARY KEY (`idUser`));
@@ -91,7 +91,45 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
-ALTER TABLE `proyecto9`.`User` 
-CHANGE COLUMN `rol` `rol` ENUM('BuscaTesoros','Colaborador','Admin','Deshabilitado') NOT NULL ;
+DELETE FROM `proyecto9`.`Log`;
+DELETE FROM `proyecto9`.`Usuario_Tesoro_Map`;
+DELETE FROM `proyecto9`.`Tesoro`;
 
-INSERT INTO `proyecto9`.`User` (`idUser`, `username`, `password`, `email`, `rol`) VALUES ('1', 'admin', 'admin', 'admin@admin.com', 'Admin');
+DELETE FROM `proyecto9`.`User`;
+
+
+
+INSERT INTO `proyecto9`.`User` (`idUser`, `username`, `password`, `email`, `rol`) 
+VALUES ('1', 'admin', 'admin', 'admin@admin.com', 'Admin'),
+('2', 'bt', 'bt', 'bt@bt.com', 'BuscaTesoros'),
+('3', 'bt1', 'bt1', 'bt1@bt1.com', 'BuscaTesoros'),
+('4', 'bt2', 'bt2', 'bt2@bt2.com', 'BuscaTesoros'),
+('5', 'bt3', 'bt3', 'bt3@bt3.com', 'BuscaTesoros'),
+('6', 'col','col', 'col@col.com', 'Colaborador'),
+('7', 'col1','col1', 'col1@col1.com',  'Colaborador');
+
+
+INSERT INTO `proyecto9`.`Tesoro` (`idTesoro`,  `nombre`,  `ciudad`,  `pais`,  `posicion`,  `codigoSecreto`,  `habilitado`,  `user_idUser`) 
+VALUES ('1','Pirata','Malaga','España','13-39','pirata',true,'6'),
+('2','Nazi','Granada','España','17-39','nazi',true,'6'),
+('3','Magico','Jaen','España','34-39','magico',true,'6'),
+('4','Cofre de Oro','Sevilla','España','47-39','cofredeoro',true,'7'),
+('5','Rubi','Cadiz','España','53-39','rubi',true,'7');
+
+INSERT INTO `proyecto9`.`Usuario_Tesoro_Map` (`user_idUser`, `Tesoro_idTesoro`)
+VALUES ('2','1'),
+('2','2'),
+('2','3'),
+('3','3'),
+('3','4'),
+('4','5'),
+('4','1');
+
+INSERT INTO `proyecto9`.`Log` (`idLog`,  `comentario` ,  `tipo` ,  `user_idUser`,  `Tesoro_idTesoro` )
+VALUES ('1','Hola don pepito','noEncontrado','2','1'),
+('2','Adios don pepito','noEncontrado','3','2'),
+('3','Ha sido duro, pero...','encontrado','3','3'),
+('4','Han destruido el tesoro :(','problemas','4','1'),
+('5','Lo conseguí','encontrado','4','4'),
+('6','Debajo de tierra no esta :(','noEncontrado','5','2');
+
