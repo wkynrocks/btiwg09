5413bd657785ead540d6f5a04017da19e49be140
Jorge Peña <nosisomes@gmail.com>
2013-11-15 10:10:20 +0100

    Me cago en la puta Quitado :D

--- ClienteProyecto9/src/java/managedBeans/Usuario.java
+++ ClienteProyecto9/src/java/managedBeans/Usuario.java
@@ -29,7 +29,6 @@ public class Usuario {
     private boolean fallologin=false;
 
     public boolean isAdmin(){
-        System.out.println("MECAGOENLAPUTA: ESto es "+usuario.getRol());
         return usuario.getRol().equals("Admin");
     }
     public boolean isFallologin() {
