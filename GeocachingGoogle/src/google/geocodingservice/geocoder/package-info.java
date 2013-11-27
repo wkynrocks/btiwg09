4389e9d421c6a35318d60dfac7ecd00a4c3a5c69
Jorge Peña <nosisomes@gmail.com>
2013-11-27 12:28:10 +0100

    Geocatching eliminado

--- GeocachingGoogle/src/google/geocodingservice/geocoder/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.5-2 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: PM.11.26 a las 08:28:44 PM CET 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://earth.google.com/kml/2.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package google.geocodingservice.geocoder;
